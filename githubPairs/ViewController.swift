8fd7cb1a967cfb542fcf09ec8011aac8a5db7835
nithya <nithya@guduri.com>
2021-06-29 13:27:41 -0400

    printed and commented

--- githubPairs/ViewController.swift
+++ githubPairs/ViewController.swift
@@ -13,8 +13,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         var hi = "hi!"
+        print(hi)
     }
-// Hi Nithya!
+    // Hi Nithya!
+    // Hi Lucy :))
 
 }
 
